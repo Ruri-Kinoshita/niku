fddb7863bb1bdec3df60befa32db4165b8b4b043
Ruri-Kinoshita <b1023081@fun.ac.jp>
2024-10-31 15:05:53 +0900

    画面遷移できた最高すぎ

--- lib/router.dart
+++ lib/router.dart
@@ -4,35 +4,38 @@ import 'package:niku/roulette.dart';
 import 'package:niku/timer.dart';
 import 'package:niku/titlepage.dart';
 
-final router = GoRouter(initialLocation: '/', routes: [
-  GoRoute(
-    path: '/',
-    name: 'title',
-    pageBuilder: (context, state) {
-      return MaterialPage(
-        key: state.pageKey,
-        child: TitlePage(),
-      );
-    },
-  ),
-  GoRoute(
-    path: '/roulette',
-    name: 'roulette',
-    pageBuilder: (context, state) {
-      return MaterialPage(
-        key: state.pageKey,
-        child: const Roulette(),
-      );
-    },
-  ),
-  GoRoute(
-    path: '/countdowntimer',
-    name: 'rcountdoentimer',
-    pageBuilder: (context, state) {
-      return MaterialPage(
-        key: state.pageKey,
-        child: CountdownTimer(),
-      );
-    },
-  ),
-]);
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      name: 'title',
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: TitlePage(),
+        );
+      },
+    ),
+    GoRoute(
+      path: '/roulette',
+      name: 'roulette',
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: const Roulette(),
+        );
+      },
+    ),
+    GoRoute(
+      path: '/countdowntimer',
+      name: 'rcountdoentimer',
+      pageBuilder: (context, state) {
+        return MaterialPage(
+          key: state.pageKey,
+          child: CountdownTimer(),
+        );
+      },
+    ),
+  ],
+);
