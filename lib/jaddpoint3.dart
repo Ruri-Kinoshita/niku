59cbac0032e94cbf8f43bf7e669de5acc2bac00d
Airi Nakanishi <b1023165@fun.ac.jp>
2024-11-10 01:10:15 +0900

    jaddpoint3画面終わったよ〜〜！！！！

--- lib/jaddpoint3.dart
+++ lib/jaddpoint3.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:niku/constant/app_color.dart';
 import 'package:niku/provider3.dart';
 import 'package:niku/round2/provider2.dart';
 import 'package:niku/round1/roulette1.dart';
@@ -21,6 +22,7 @@ class _Jaddpoint3State extends ConsumerState<Jaddpoint3> {
 
   @override
   Widget build(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
     final prompts = ref.watch(promptProvider3); // お題プロバイダを監視
     final jRolePlayerName =
         ref.watch(userProviderProvider).firstWhere((User user) {
@@ -29,115 +31,237 @@ class _Jaddpoint3State extends ConsumerState<Jaddpoint3> {
 
     return Scaffold(
       appBar: AppBar(title: Text('選ばれたお題')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-                '審査員役の${jRolePlayerName.name ?? '〇〇'}さん'), // "J" 役の名前またはプレースホルダーを表示
-            Text('これはなんの料理でしょうか？'),
-            Text('2回まで回答できます'),
-            Text('1回目で正解したら◎，2回目で正解したら○，不正解なら×を押してください'),
-            Center(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: _buttonDisabled
-                        ? null
-                        : () {
-                            ref
-                                .read(userProviderProvider.notifier)
-                                .addScore(2, "J");
-                            setState(() {
-                              _judgePressed = "2点加算";
-                              _buttonDisabled = true; // ボタンを無効化
-                            });
-                          },
-                    child: const Text('◎'),
-                    style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(),
-                      padding: EdgeInsets.all(60),
-                    ),
+      body: Stack(
+        children: [
+          Opacity(
+            opacity: 0.7,
+            child: Container(
+                width: deviceWidth * 0.4,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/leaf_left.png'),
+                    alignment: Alignment.bottomLeft,
                   ),
-                  ElevatedButton(
-                    onPressed: _buttonDisabled
-                        ? null
-                        : () {
-                            ref
-                                .read(userProviderProvider.notifier)
-                                .addScore(1, "J");
-                            setState(() {
-                              _judgePressed = "1点加算";
-                              _buttonDisabled = true; // ボタンを無効化
-                            });
-                          },
-                    child: const Text('○'),
-                    style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(),
-                      padding: EdgeInsets.all(60),
-                    ),
+                )),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Opacity(
+              opacity: 0.7,
+              child: Container(
+                width: deviceWidth * 0.62,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/leaf_right.png'),
+                    alignment: Alignment.bottomRight,
+                    fit: BoxFit.contain,
                   ),
-                  ElevatedButton(
-                    onPressed: _buttonDisabled
-                        ? null
-                        : () {
-                            setState(() {
-                              _judgePressed = "ざまあ";
-                              _buttonDisabled = true; // ボタンを無効化
-                            });
-                          },
-                    child: const Text('×'),
-                    style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(),
-                      padding: EdgeInsets.all(60),
-                    ),
-                  ),
-                ],
+                ),
               ),
             ),
-            if (_buttonPressed) ...[
-              Text(
-                'お客さん: ${prompts[0]}',
-                style: TextStyle(fontSize: 24),
-              ),
-              SizedBox(height: 20),
-              Text(
-                '料理: ${prompts[1]}',
-                style: TextStyle(fontSize: 24),
-              ),
-            ],
-            SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: _buttonPressed
-                  ? null
-                  : () {
-                      setState(() {
-                        _buttonPressed = true;
-                      });
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '審査員役の${jRolePlayerName.name ?? '〇〇'}さん',
+                  style: const TextStyle(fontSize: 30),
+                ), // "J" 役の名前またはプレースホルダーを表示
+                const Text(
+                  'これはなんの料理でしょうか？',
+                  style: TextStyle(fontSize: 30),
+                ),
+                const Text(
+                  '2回まで回答できます',
+                  style: TextStyle(fontSize: 30),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      '1回目で正解したら',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      '◎',
+                      style:
+                          TextStyle(fontSize: 30, color: Colors.red.shade800),
+                    ),
+                    const Text(
+                      '，2回目で正解したら',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      '○',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.pink.shade600,
+                      ),
+                    ),
+                    const Text(
+                      '，不正解なら',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    Text(
+                      '×',
+                      style:
+                          TextStyle(fontSize: 30, color: Colors.blue.shade900),
+                    ),
+                    const Text(
+                      'を押してください',
+                      style: TextStyle(fontSize: 30),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: _buttonPressed
+                      ? null
+                      : () {
+                          setState(() {
+                            _buttonPressed = true;
+                          });
+                        },
+                  child: const Text('答えを表示'),
+                ),
+                if (_buttonPressed) ...[
+                  const SizedBox(height: 20),
+                  Text(
+                    '料理: ${prompts[1]}',
+                    style: const TextStyle(fontSize: 24),
+                  ),
+                ],
+                const SizedBox(height: 30),
+                Center(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: _buttonDisabled
+                            ? null
+                            : () {
+                                ref
+                                    .read(userProviderProvider.notifier)
+                                    .addScore(2, "J");
+                                setState(() {
+                                  _judgePressed =
+                                      '審査員の${jRolePlayerName.name ?? '〇〇'}さんに2ポイント加算されました。';
+                                  _buttonDisabled = true; // ボタンを無効化
+                                });
+                              },
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(30),
+                          backgroundColor: Colors.white,
+                          side:
+                              BorderSide(color: Colors.red.shade800, width: 6),
+                        ),
+                        child: Text(
+                          '◎',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 80,
+                              color: Colors.red.shade800),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: _buttonDisabled
+                            ? null
+                            : () {
+                                ref
+                                    .read(userProviderProvider.notifier)
+                                    .addScore(1, "J");
+                                setState(() {
+                                  _judgePressed =
+                                      '審査員の${jRolePlayerName.name ?? '〇〇'}さんに1ポイント加算されました。';
+                                  _buttonDisabled = true; // ボタンを無効化
+                                });
+                              },
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(35),
+                          backgroundColor: Colors.white,
+                          side:
+                              BorderSide(color: Colors.pink.shade600, width: 6),
+                        ),
+                        child: Center(
+                          child: Text(
+                            '○',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 70,
+                              color: Colors.pink.shade600,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20,
+                      ),
+                      ElevatedButton(
+                        onPressed: _buttonDisabled
+                            ? null
+                            : () {
+                                setState(() {
+                                  _judgePressed = "ざんねん！0ポイントです。";
+                                  _buttonDisabled = true; // ボタンを無効化
+                                });
+                              },
+                        style: ElevatedButton.styleFrom(
+                          shape: const CircleBorder(),
+                          padding: const EdgeInsets.all(30),
+                          backgroundColor: Colors.white,
+                          side:
+                              BorderSide(color: Colors.blue.shade900, width: 6),
+                        ),
+                        child: Text(
+                          '×',
+                          style: TextStyle(
+                              fontWeight: FontWeight.w900,
+                              fontSize: 80,
+                              color: Colors.blue.shade900),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 20),
+                if (_judgePressed.isNotEmpty)
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        _judgePressed,
+                        style: const TextStyle(fontSize: 24),
+                      ),
+                      // Text(ref
+                      //     .watch(userProviderProvider)
+                      //     .firstWhere((test) => test.role == 'J')
+                      //     .score
+                      //     .toString())
+                      const SizedBox(height: 20),
+                    ],
+                  ),
+                ElevatedButton(
+                    onPressed: () {
+                      context.go('/teampresentation3');
                     },
-              child: Text('お題を表示'),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: AppColor.ui.lightgreen,
+                      side: BorderSide(
+                          width: 3, color: Colors.green.withOpacity(1)),
+                      fixedSize: const Size(100, 50),
+                    ),
+                    child: Text('次へ'))
+              ],
             ),
-            SizedBox(height: 50),
-            if (_judgePressed.isNotEmpty)
-              Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(_judgePressed),
-                  Text(ref
-                      .watch(userProviderProvider)
-                      .firstWhere((test) => test.role == 'J')
-                      .score
-                      .toString())
-                ],
-              ),
-            ElevatedButton(
-                onPressed: () {
-                  context.go('/teampresentation3');
-                },
-                child: Text('次へ'))
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
