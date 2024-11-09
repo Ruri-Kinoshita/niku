f541ef4152d81422d0cc5dfb359a5350fb737759
Airi Nakanishi <b1023165@fun.ac.jp>
2024-11-09 21:15:41 +0900

    草

--- lib/titlepage.dart
+++ lib/titlepage.dart
@@ -14,27 +14,74 @@ class _TitlePageState extends State<TitlePage> {
     final deviceWidth = MediaQuery.of(context).size.width;
     final deviceHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-        body: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      body: Stack(
         children: [
-          Image(
-            height: deviceHeight * 0.5,
-            image: AssetImage('assets/images/titlelogo.png'),
+          Opacity(
+            opacity: 0.7,
+            child: Container(
+                width: deviceWidth * 0.4,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/leaf_left.png'),
+                    alignment: Alignment.bottomLeft,
+                  ),
+                )),
           ),
-          Text(
-            '更新しないでね！！',
-            style: TextStyle(
-              color: AppColor.text.darkred,
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Opacity(
+              opacity: 0.7,
+              child: Container(
+                width: deviceWidth * 0.62,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/leaf_right.png'),
+                    alignment: Alignment.bottomRight,
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
             ),
           ),
-          ElevatedButton(
-              onPressed: () {
-                context.go('/usersetting');
-              },
-              child: Text('ユーザー登録に遷移するはず')),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                SizedBox(
+                  height: deviceHeight * 0.01,
+                ),
+                Image(
+                  height: deviceHeight * 0.4,
+                  image: const AssetImage('assets/images/titlelogo.png'),
+                ),
+                SizedBox(
+                  width: deviceWidth * 0.3,
+                  child: Card(
+                    color: AppColor.ui.lightred,
+                    child: Center(
+                      child: Text(
+                        '注意：ブラウザの更新ボタンは押さずに遊んでね',
+                        style: TextStyle(
+                          color: AppColor.text.darkred,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      context.go('/usersetting');
+                    },
+                    style: ElevatedButton.styleFrom(
+                      //primary: AppColor.ui.lightgreen,
+                      minimumSize: Size(50, 10),
+                    ),
+                    child: const Text('次へ')),
+              ],
+            ),
+          ),
         ],
       ),
-    ));
+    );
   }
 }
